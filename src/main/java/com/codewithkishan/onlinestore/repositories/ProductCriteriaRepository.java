3e159a81e2029dcd27d0a102714a5427bef1b7dd
Kishan Singh <kishansingh26195@gmail.com>
2026-07-04 23:10:48 +0530

    fix typo

--- src/main/java/com/codewithkishan/onlinestore/repositories/ProductCriteriaRepository.java
+++ src/main/java/com/codewithkishan/onlinestore/repositories/ProductCriteriaRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.math.BigDecimal;
 import java.util.List;
 
-public interface ProductCriteriaRepository imp {
+public interface ProductCriteriaRepository {
 
     List<Product> findProductByCriteria(String name, BigDecimal minPrice, BigDecimal maxPrice);
 }
